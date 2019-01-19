3060c419db85177e50066254d1a4b7d005b3393a
nathan pointer <nwpointer@gmail.com>
2019-01-18 18:09:30 -0800

    gitignore

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
-const Metalsmith  = require('metalsmith');
-const collections = require('metalsmith-collections');
-const layouts     = require('metalsmith-layouts');
-const markdown    = require('metalsmith-markdown');
-const permalinks  = require('metalsmith-permalinks');
+var Metalsmith  = require('metalsmith');
+var markdown    = require('metalsmith-markdown');
+var layouts     = require('metalsmith-layouts');
+var permalinks  = require('metalsmith-permalinks');
+var collections = require('metalsmith-collections');
+var watch = require('metalsmith-watch');
 
-Metalsmith(__dirname)         // __dirname defined by node.js:
-  .metadata({                 // add any variable you want
-    sitename: "My Static Site & Blog",
-    siteurl: "http://example.com/",
-    description: "It's about saying »Hello« to the world.",
-    generatorname: "Metalsmith",
-    generatorurl: "http://metalsmith.io/"
-  })
-  .source('./src')            // source directory
-  .destination('./build')     // destination directory
-  .clean(true)                // clean destination before
-  .use(collections({          // group all blog posts by internally
-    posts: 'posts/*.md'       // adding key 'collections':'posts'
-  }))                         // use `collections.posts` in layouts
-  .use(markdown())            // transpile all md into html
-  .use(permalinks({           // change URLs to permalink URLs
-    relative: false           // put css only in /css
-  }))
-  .use(layouts())             // wrap layouts around html
-  .build(function(err) {      // build process
-    if (err) throw err;       // error handling is required
-  });
+Metalsmith(__dirname)
+    .metadata({
+        title: "My Static Site & Blog",
+        description: "It's about saying »Hello« to the World.",
+        generator: "Metalsmith",
+        url: "http://www.metalsmith.io/"
+    })
+    .source('./src')
+    .destination('./dist')
+    .clean(false)
+    .use(markdown())
+    .use(permalinks({ relative: true }))
+    .use(collections({
+        posts: 'posts/*.md'
+    }))
+    .use(layouts())
+    .use(maybeWatch())
+    .build(function(err, files) {
+        if (err) { throw err; }
+        console.log('done')
+    });
+
+function maybeWatch(){
+    const active = process.argv.includes('-w') || process.argv.includes('watch')
+    if(active){
+        return watch({
+            paths: {
+                "${source}/**/*": true,
+                "layouts/**/*": "**/*.md",
+            },
+            livereload: true,
+        })
+    }
+}
