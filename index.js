2e85ae58d71c95ebe4e036f38df5e8f99576246e
nathan pointer <nwpointer@gmail.com>
2019-01-19 18:49:18 -0800

    changed templating system

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
 var Metalsmith  = require('metalsmith');
-var markdown    = require('metalsmith-markdown');
+var inplace = require('metalsmith-in-place');
 var layouts     = require('metalsmith-layouts');
 var permalinks  = require('metalsmith-permalinks');
 var collections = require('metalsmith-collections');
 var watch = require('metalsmith-watch');
 
-Metalsmith(__dirname)
-    .metadata({
-        title: "My Static Site & Blog",
-        description: "It's about saying »Hello« to the World.",
-        generator: "Metalsmith",
-        url: "http://www.metalsmith.io/"
-    })
-    .source('./src')
-    .destination('./dist')
-    .clean(false)
-    .use(markdown())
-    .use(permalinks({ relative: true }))
-    .use(collections({
-        posts: 'posts/*.md'
-    }))
-    .use(layouts())
-    .use(maybeWatch())
-    .build(function(err, files) {
-        if (err) { throw err; }
-        console.log('done')
-    });
+const toUpper = function (string) {
+    return string.toUpperCase();
+};
+
+const spaceToDash = function (string) {
+    return string.replace(/\s+/g, "-");
+};
+
+const templateConfig = {
+    engineOptions: {
+        filters: {
+            toUpper: toUpper,
+            spaceToDash: spaceToDash
+        }
+    },
+    suppressNoFilesError: true
+};
+
 
 function maybeWatch(){
     const active = process.argv.includes('-w') || process.argv.includes('watch')
@@ -39,3 +36,43 @@ function maybeWatch(){
         })
     }
 }
+
+function simplePublished(opts){
+    opts = opts || {};
+    return function (files, metalsmith, done) {
+        Object.keys(files).map((file)=>{
+            var data = files[file];
+            var pub = typeof data.published !== 'undefined' ? data.published : true;
+            console.log({
+                file,
+                q: typeof data.published !== 'undefined'
+            })
+            if(!pub){
+                delete files[file];
+            }
+        })
+        done()
+    }
+}
+
+Metalsmith(__dirname)
+    .metadata({
+        title: "My Static Site & Blog",
+        description: "It's about saying »Hello« to the World.",
+        generator: "Metalsmith",
+        url: "http://www.metalsmith.io/"
+    })
+    .source('./src')
+    .destination('./dist')
+    .clean(true)
+    .use(simplePublished())
+    .use(permalinks({ relative: true }))
+    .use(collections({ posts: 'posts/*.md' }))
+    .use(inplace(templateConfig))
+    .use(layouts(templateConfig))
+    .use(maybeWatch())
+    .build(function(err, files) {
+        if (err) { throw err; }
+        console.log('done')
+    });
+
